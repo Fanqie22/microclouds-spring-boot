07b43248d540b84e8f31d5190844fcbfdc2b4ebd
Fanqie22 <1393203396@qq.com>
2019-01-02 22:22:18 +0800

    获取文件MD5接口,通过比较MD5判断两个文件是否相同

---
+++ src/main/java/com/microclouds/common/util/GetHash.java
@@ -0,0 +1,23 @@
+package com.microclouds.common.util;
+
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static sun.security.pkcs11.wrapper.Functions.toHexString;
+
+public class GetHash {
+    public static String getFileMD5(String fileName, String hashType) throws IOException, NoSuchAlgorithmException {
+        File f = new File(fileName);
+        InputStream ins = new FileInputStream(f);
+        byte[] buffer = new byte[8192];
+        MessageDigest md5 = MessageDigest.getInstance(hashType);
+        int len;
+        while ((len = ins.read(buffer)) != -1) {
+            md5.update(buffer, 0, len);
+        }
+        ins.close();
+        return toHexString(md5.digest());
+    }
+
+}
--- src/test/java/com/microclouds/mainTest.java
+++ src/test/java/com/microclouds/mainTest.java
@@ -1,7 +1,6 @@
 package com.microclouds;
 
 public class mainTest {
-    public void main(String[] args){
-
+    public static void main(String[] args) {
     }
 }
